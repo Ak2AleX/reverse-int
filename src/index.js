4de1675306de976e46a9cfc50f2e790684ee880c
Ak2AleX <ak2movlex@gmail.com>
2026-05-10 19:40:20 +0300

    feat: solution

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
-module.exports = function reverse (n) {
-    return (
-        parseFloat(n.toString().split('').reverse().join(''))     
-    )                 
-}
+module.exports = function reverse(n) {
+  const str = n.toString();
+  const arr = str.split('');
+  const reversedArr = arr.reverse();
+  const reversedStr = reversedArr.join('');
+  const result = parseInt(reversedStr, 10);
+  return result;
+};
